cd5fa48db216e40b82f2b688781d7fa5ba7058af
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-30 12:57:42 +0100

    chore: install and initialize ESLint, fix all linting issues

--- part3/contacts.js
+++ part3/contacts.js
@@ -1,32 +1,32 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 // Schema
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 3,
-    required: [true, "Contact name is required"],
+    required: [true, 'Contact name is required'],
   },
   number: {
     type: String,
-    required: [true, "Contact phone number is required"],
+    required: [true, 'Contact phone number is required'],
     validate: {
       validator: function (value) {
-        return /^(\d{2}-\d+|\d{3}-\d+)$/.test(value);
+        return /^(\d{2}-\d+|\d{3}-\d+)$/.test(value)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
-});
+})
 
-contactSchema.set("toJSON", {
+contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-const Contact = mongoose.model("Contact", contactSchema);
+const Contact = mongoose.model('Contact', contactSchema)
 
-module.exports = Contact;
+module.exports = Contact
--- part3/index.js
+++ part3/index.js
@@ -1,227 +1,221 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const app = express();
-const PORT = process.env.PORT || 3001;
-const connectDB = require("./config/mongo.js");
-const Contact = require("./contacts.js");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+const PORT = process.env.PORT || 3001
+const connectDB = require('./config/mongo.js')
+const Contact = require('./contacts.js')
 
-connectDB();
+connectDB()
 
-app.use(cors());
+app.use(cors())
 
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
     id: 4,
-    name: "Marie Poppendick",
-    number: "39-23-6423122",
+    name: 'Marie Poppendick',
+    number: '39-23-6423122',
   },
-];
+]
 
-morgan.token("req-body", (req) => {
-  return req.method === "POST" ? JSON.stringify(req.body) : "";
-});
-morgan.token("method", (req) => req.method);
-morgan.token("url", (req) => req.url);
-morgan.token("status", (req, res) => res.statusCode);
+morgan.token('req-body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
+morgan.token('method', (req) => req.method)
+morgan.token('url', (req) => req.url)
+morgan.token('status', (req, res) => res.statusCode)
 const customFormat =
-  ":method :url :status :res[content-length] :response-time ms - :req-body";
+  ':method :url :status :res[content-length] :response-time ms - :req-body'
 
-app.set("trust proxy", true);
+app.set('trust proxy', true)
 
-app.use(morgan(customFormat));
+app.use(morgan(customFormat))
 
-app.use(express.static("dist"));
-// app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static('dist'))
+// app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // Desc      Home Page
 // Route     GET http://localhost:3001/
-app.get("/", (req, res, next) => {
+app.get('/', (req, res, next) => {
   try {
-    res.send("Your PhoneBook");
+    res.send('Your PhoneBook')
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 // Desc      Phonebook Information
 // Route     GET http://localhost:3001/info/
-app.get("/info", (req, res, next) => {
+app.get('/info', (req, res, next) => {
   try {
-    const date = new Date();
+    const date = new Date()
 
     res.send(
       `<div>
         <p>Phonebook has info for ${persons.length} people</p>
          <p>${date.toString()}</p> 
       </div>`
-    );
+    )
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 // Desc      Phonebook Contacts
 // Route     GET http://localhost:3001/api/persons/
-app.get("/api/persons", (req, res, next) => {
+app.get('/api/persons', (req, res, next) => {
   try {
     Contact.find({})
       .then((data) => {
-        res.json(data);
+        res.json(data)
       })
       .catch((err) => {
-        console.error(err);
-        res.send(err);
-      });
+        console.error(err)
+        res.send(err)
+      })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 // Desc      Person Contact
 // Route     GET http://localhost:3001/api/persons/5
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
     Contact.findById(id)
       .then((person) => {
         if (person) {
-          res.json(person);
+          res.json(person)
         } else {
-          res.status(404).end();
+          res.status(404).end()
         }
       })
       .catch((err) => {
-        console.error(err);
-        response.status(400).send({ error: "malformatted id" });
-      });
+        console.error(err)
+        res.status(400).send({ error: 'malformatted id' })
+      })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
-
-const generateId = () => {
-  const maxId =
-    persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
-  return maxId + 1;
-};
+})
 
 // Desc      Add new Contact
 // Route     POST http://localhost:3001/api/persons
-app.post("/api/persons", async (req, res, next) => {
+app.post('/api/persons', async (req, res, next) => {
   try {
-    const { name, number } = req.body;
+    const { name, number } = req.body
     if (!name) {
       res.status(400).json({
-        error: "name is missing",
-      });
+        error: 'name is missing',
+      })
     } else if (!number) {
       res.status(400).json({
-        error: "number is missing",
-      });
+        error: 'number is missing',
+      })
     }
 
     const contact = new Contact({
       name,
       number,
-    });
+    })
 
     try {
-      const savedContact = await contact.save();
+      const savedContact = await contact.save()
 
-      res.status(201).json(savedContact);
+      res.status(201).json(savedContact)
     } catch (error) {
-      next(error);
-      res.status(400).json({ success: false, error });
+      next(error)
+      res.status(400).json({ success: false, error })
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 // Desc      Update Contact
 // Route     PUT http://localhost:3001/api/persons:id
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
-  const { id } = request.params;
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+  const { id } = req.params
 
   if (!name) {
     res.status(400).json({
-      error: "name is missing",
-    });
+      error: 'name is missing',
+    })
   } else if (!number) {
     res.status(400).json({
-      error: "number is missing",
-    });
+      error: 'number is missing',
+    })
   }
 
   const contact = {
     name,
     number,
-  };
+  }
 
   Contact.findByIdAndUpdate(id, contact, { new: true, runValidators: true })
     .then((contact) => {
-      response.json(contact);
+      res.json(contact)
     })
     .catch((error) => {
-      next(error);
-      console.error(error);
-    });
-});
+      next(error)
+      console.error(error)
+    })
+})
 
 // Desc      Delete Personnotes
 // Route     DELETE http://localhost:3001/api/persons/5
-app.delete("/api/persons/:id", (req, res, next) => {
-  const { id } = req.params;
+app.delete('/api/persons/:id', (req, res, next) => {
+  const { id } = req.params
 
   Contact.findByIdAndDelete(id)
-    .then((result) => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(
     `Server is running on http://localhost:${PORT} ...betta go catch it`
-  );
-});
+  )
+})
